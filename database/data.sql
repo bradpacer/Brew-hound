79490b496d8adb6431479f570b88abdf609fac61
Nick Connor <nickconnor52@gmail.com>
2017-11-17 13:28:43 -0500

    Created DB Schema, began DB data entry

--- database/data.sql
+++ database/data.sql
@@ -6,4 +6,19 @@ BEGIN;
 
 -- INSERT statements go here
 
+-- USERS
+INSERT INTO users(username, password, role) VALUES (admin_user, password1, admin);
+
+-- BREWERY
+INSERT INTO brewery(username, password, role) VALUES (admin_user, password1, admin);
+
+-- BEER
+INSERT INTO beer(username, password, role) VALUES (admin_user, password1, admin);
+
+-- REVIEW
+INSERT INTO review(username, password, role) VALUES (admin_user, password1, admin);
+
+-- LOCATION
+INSERT INTO location(username, password, role) VALUES (admin_user, password1, admin);
+
 COMMIT;
--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,101 @@
 -- *************************************************************************************************
 
 BEGIN;
-
+  
 -- CREATE statements go here
+
+-- TABLE user
+CREATE SEQUENCE users_users_id_seq
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE TABLE users (
+	user_id INTEGER DEFAULT nextval('users_users_id_seq'::regclass) NOT NULL,
+	username varchar(30) NOT NULL,
+	password varchar(80) NOT NULL,
+	role varchar(20) NOT NULL,
+	
+	CONSTRAINT pk_users_users_id PRIMARY KEY (user_id)
+);
+
+-- TABLE brewery 
+CREATE SEQUENCE brewery_brewery_id_seq
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE TABLE brewery (
+	brewery_id INTEGER DEFAULT nextval('brewery_brewery_id_seq'::regclass) NOT NULL,
+	name varchar(30) NOT NULL,
+	location_id integer NOT NULL,
+	address varchar(80) NOT NULL,
+	latitude varchar(30) NOT NULL,
+	longitude varchar(30) NOT NULL,
+	description TEXT NOT NULL,
+	year_founded INTEGER NOT NULL,
+	image_path varchar(30) NOT NULL,
+	
+	CONSTRAINT pk_brewery_brewery_id PRIMARY KEY (brewery_id),
+	CONSTRAINT fk_brewery_location FOREIGN KEY (location_id) REFERENCES location (location_id)
+);
+
+-- TABLE beer 
+CREATE SEQUENCE beer_beer_id_seq
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE TABLE beer (
+	beer_id INTEGER DEFAULT nextval('beer_beer_id_seq'::regclass) NOT NULL,
+	brewery_id INTEGER NOT NULL,
+	name varchar(30) NOT NULL,
+	description TEXT NOT NULL,
+	beer_type varchar(20) NOT NULL,
+	abv REAL NOT NULL,
+	ibu REAL,
+	glass_type varchar(30),
+	
+	CONSTRAINT pk_beer_beer_id PRIMARY KEY (beer_id),
+	CONSTRAINT fk_beer_brewery FOREIGN KEY (brewery_id) REFERENCES brewery (brewery_id)
+);
+
+-- TABLE review 
+CREATE SEQUENCE review_review_id_seq
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE TABLE review (
+	review_id INTEGER DEFAULT nextval('review_review_id_seq'::regclass) NOT NULL,
+	beer_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+	description TEXT NOT NULL,
+	rating INTEGER NOT NULL,
+	
+	CONSTRAINT pk_review_review_id PRIMARY KEY (review_id),
+	CONSTRAINT fk_review_beer FOREIGN KEY (beer_id) REFERENCES beer (beer_id),
+	CONSTRAINT fk_review_users FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+-- TABLE location 
+CREATE SEQUENCE location_location_id_seq
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE TABLE review (
+	location_id INTEGER DEFAULT nextval('location_location_id_seq'::regclass) NOT NULL,
+	city varchar(30) NOT NULL,
+	state varchar(30) NOT NULL,
+	
+	CONSTRAINT pk_location_location_id PRIMARY KEY (location_id),
+);
+
 
 COMMIT;
